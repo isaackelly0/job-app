f1ce52024277bde1c3d0e455716f9037fbae4f13
Isaac Kelly <isaacmkelly0@gmail.com>
2021-03-29 16:26:03 -0500

    added router to app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from "./components/Home";
+import Page from "./components/Page";
+import javascript from "./components/data/javascript";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+          <Link to="/front-end">HTML/CSS</Link>
+
+          </li>
+          <li>
+          <Link to="/javascript">Javascript</Link>
+
+          </li>
+
+        </ul>
+      </nav>
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route path="/javascript">
+          <Page list={javascript}/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
