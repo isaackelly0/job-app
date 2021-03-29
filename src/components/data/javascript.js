775ed6d93b50a7f415dc76edbd543ff2fd504e4d
Isaac Kelly <isaacmkelly0@gmail.com>
2021-03-29 16:28:05 -0500

    arrays of objects for data rendering

---
+++ src/components/data/javascript.js
@@ -0,0 +1,19 @@
+let jsProjects = [
+    {
+        name: "Something basic",
+        pic: "",
+        alt: "duck",
+        description: ";)",
+        page: "Don't bother clicking",
+        github: "seriously don't"
+    },
+    {
+        name:"something else",
+        pic:"",
+        alt:"dodge",
+        description: ":[",
+        page: "blah",
+        github: "blahblahblah"
+    }
+]
+export default jsProjects;
