a9cf7e1d2f2619e07f4d0a0d9114de774d30658e
Isaac Kelly <isaacmkelly0@gmail.com>
2021-05-11 16:54:08 -0500

    added new images and updated descriptions for projects on display

--- src/components/data/front-end.js
+++ src/components/data/front-end.js
@@ -11,7 +11,7 @@ let frontProjects = [
         name: 'VideoTube',
         pic: videotube,
         alt: 'VideoTube',
-        description: 'Mock-up youtube page',
+        description: 'Ew. Okay, admittedly this definitely isn\'t one of my finest works, but that is kind of the point. This is the first project I made with base html and css from the ground up to imitate a Youtube page with my introductory knowledge. I would definitely do it better now but it works as a nice baseline.',
         page: 'https://isaackelly0.github.io/youtube-page/',
         github: 'https://github.com/isaackelly0/youtube-page'
     },
@@ -19,7 +19,7 @@ let frontProjects = [
         name: 'Mint page',
         pic: mint,
         alt: 'Mint',
-        description: 'Inuit Turbotax mock-up page',
+        description: 'Ah, that\'s a tad better. This is my first use of html forms, and even though the css is done a bit roughly you can see my grasp here is an improvement. Naturally the page doesn\'t actually do anything since there\'s no Javascript but that\'s most of this section.',
         page: 'https://isaackelly0.github.io/mint-page/',
         github: 'https://github.com/isaackelly0/mint-page'
     },
@@ -27,7 +27,7 @@ let frontProjects = [
         name: 'New York Times',
         pic: nyt,
         alt: 'nyt',
-        description: 'New York times article',
+        description: 'Now we\'re cooking with gas! This one was built as a mock-up of a New York Times web article after I learned about grid and flex in css. I figured out a lot about how nesting elements in html makes it a lot easier to manipulate when styling, and how much pre-planning can pay off.',
         page: 'https://isaackelly0.github.io/nyt/',
         github: 'https://github.com/isaackelly0/nyt'
     },
@@ -35,7 +35,7 @@ let frontProjects = [
         name: 'Apple Page',
         pic: apple,
         alt: 'apple mock up',
-        description: 'immitation apple page',
+        description: 'A bit more practice with positioning here, tried my hand at linear gradients too. This is a mock-up of an archived Apple website, I figured out quite a bit about imitating the styles of other websites this way and how to manipulate child elements relative to their parents\'.',
         page: 'https://isaackelly0.github.io/apple-page/',
         github: 'https://github.com/isaackelly0/apple-page'
     },
@@ -43,7 +43,7 @@ let frontProjects = [
         name: 'The Next Web',
         pic: nextweb,
         alt: 'nextweb',
-        description: 'first-time responsive website',
+        description: 'You knew this was coming eventually, we all did. If you\'re working in the front-end in the era of smart phones, you need to work responsively. This is my version of The Next Web themed around Magic the Gathering to be responsive to the screen size.',
         page: 'https://isaackelly0.github.io/nextweb/',
         github: 'https://github.com/isaackelly0/nextweb'
     },
@@ -51,7 +51,7 @@ let frontProjects = [
         name: 'Newsweek',
         pic: newsweek,
         alt: 'newsweek',
-        description: 'testing bootstrap',
+        description: 'Ahh, now I don\'t have to do it myself anymore! Joking of course(except not really), this is my first foray into using bootstrap to build a Newsweek web page. I mainly had to do the styling myself but bootstrap handled pretty much all the positioning.',
         page: 'https://isaackelly0.github.io/newsweek/',
         github: 'https://github.com/isaackelly0/newsweek'
     },
@@ -59,7 +59,7 @@ let frontProjects = [
         name: 'Odin Framework',
         pic: odin,
         alt: 'odin',
-        description: 'testing personal framework',
+        description: 'For my last trick, I tried out sass to build my own styling framework. I quite like sass a lot since it lets me nest things in parent elements, and I decided to dedicate this last site to the one that got me this far, The Odin Project. I decided to challenge myself a bit this time and mainly reserved my google searches for things like color, font, and what other features sass had. For everything else, I used everything I had learned up to this point to build this.',
         page: 'https://github.com/isaackelly0/odin',
         github: 'https://github.com/isaackelly0/framework'
     }
--- src/components/data/javascript.js
+++ src/components/data/javascript.js
@@ -1,21 +1,24 @@
 import calculator from '../images/calculator.png';
 import sketch from '../images/sketch1.gif';
 import weather from '../images/weather.gif';
+import cv from '../images/cv.gif';
 import memory from '../images/memory.gif';
+import cart from '../images/cart.png';
+import counter from '../images/life-counter.gif';
 let jsProjects = [
     {
         name: "Etch-a-sketch",
         pic: sketch,
-        alt: "duck",
-        description: "It's an etch-a-sketch",
+        alt: "etch-a-sketch",
+        description: "This is my first use of javascript for DOM manipulation, and even though I would probably do it differently these days, I'm still pretty proud of it. Mouse-over events cause the background of the sketch to change color, and you can scale the sketch pad how you\'d like.",
         page: "https://isaackelly0.github.io/etch-a-sketch/",
         github: "https://github.com/isaackelly0/etch-a-sketch"
     },
     {
         name:"Calculator",
         pic:calculator,
-        alt:"dodge",
-        description: "It's a calculator",
+        alt:"calculator",
+        description: "This is a pretty basic calculator on web page, not much to really say there to be honest.",
         page: "https://isaackelly0.github.io/calculator/",
         github: "https://github.com/isaackelly0/calculator"
     },
@@ -23,40 +26,40 @@ let jsProjects = [
         name: 'Weather App',
         pic: weather,
         alt: 'Weather',
-        description: 'ooh look a cloud',
-        page: '',
+        description: 'This project handles two API requests based on user input. When the user types in a city name the site reports back the general weather patterns and fetches a relevant gif from giphy for the background.',
+        page: 'https://isaackelly0.github.io/weather-app/',
         github: 'https://github.com/isaackelly0/weather-app'
     },
     {
         name: 'CV Application',
-        pic: '',
+        pic: cv,
         alt: 'Job application',
-        description: 'ooh look a resume',
-        page: '',
+        description: 'This is my first project using the React framework, and while I would add an area or do it differently knowing what I do now, I felt it representative to keep it as is. This primarily uses the state of the first component to mount a new one with props decided by that state.',
+        page: 'https://isaackelly0.github.io/cv-project/',
         github: 'https://github.com/isaackelly0/cv-project'
     },
     {
         name: 'Memory Card',
         pic: memory,
         alt: 'cards',
-        description: 'memory game using hooks',
+        description: 'Getting a little more familiar with React features here, I created a memorization shuffler. This project was meant to familiarize me with React\'s hooks but honestly the harder part was figuring out how to randomize everything while keeping the right components mounted.',
         page: 'http://isaackelly0.github.io/memory-card',
         github: 'https://github.com/isaackelly0/memory-card'
     },
     {
         name: 'Shopping Cart',
-        pic: '',
+        pic: cart,
         alt: 'cart',
-        description: "an app testing react's router" ,
-        page: '',
+        description: "This one is kind of straight forward, using React\'s router to navigate through different pages on a website. It totally didn\'t inspire this one in any way at all. No siree. Also this allowed me to test how objects work in state." ,
+        page: 'http://isaackelly0.github.io/shopping-cart',
         github: 'https://github.com/isaackelly0/shopping-cart'
     },
     {
         name: 'Life Counter',
-        pic: '',
-        alt: '',
-        description: 'testing hooks for mtg life counter',
-        page: '',
+        pic: counter,
+        alt: 'counter',
+        description: 'Okay, this one was more for fun than anything. See, my friends and I play a form of Magic the Gathering called Commander, and while we usually have an app keep track of scores, not many can keep track of more than four people. Add on to that the fact that you need to keep track of which specific person damaged you with their commander and keeping track of things became a headache. So I made this and they still won\'t let me win.',
+        page: 'http://isaackelly0.github.io/life-counter',
         github: 'https://github.com/isaackelly0/life-counter'
     }
 ]
