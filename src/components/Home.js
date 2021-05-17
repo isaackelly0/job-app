8434a22c1e8a234284661fb559105c2bfd8b9e94
Isaac Kelly <isaacmkelly0@gmail.com>
2021-05-17 13:47:52 -0500

    added profile picture to welcome page and finished home's appearance

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 import './Home.css';
+import selfie from './images/selfie.jpg';
 const Home = () => {
     return(
         <div className="row welcome">
-            <div className="col-6">
+            <div className="col-6 profile">
                 <div className="row">
-                    <span className="col-4">Picture</span>
-                    <span className="col-6">Add some filler content here to see how much of the text box gets filled up so I can see how it looks later</span>
+                    <span className="col-lg-4 col-md-12 picture"> <img src={selfie} alt="profile pic"/> </span>
+                    <span className="col-lg-6 col-md-12">Hi there! If you're visiting this site I'm assuming you're either someone who's seen my application or someone I know that I'm just showing off too. You can find all my projects on github but I thought this might make it easier to navigate for the sake of convenience, feel free to use the navbar at the top to look around!</span>
                 </div>
             </div>
         </div>
