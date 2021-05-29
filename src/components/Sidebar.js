363e126ade1e2fdb40aaf3d85737c0a0bd3dfea7
Isaac Kelly <isaacmkelly0@gmail.com>
2021-05-29 14:16:26 -0500

    added hashrouter to fix link issue

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,5 @@
 import uniqid from 'uniqid';
+import {HashLink as Link} from 'react-router-hash-link';
 import "./Sidebar.css"
 const Sidebar = props => {
     return(
@@ -6,7 +7,7 @@ const Sidebar = props => {
             <ul>
                 {props.list.map(item=>{
                     let id = "#" + item;
-                    return<li key={uniqid()}><a href={id}>{item}</a></li>
+                    return<li key={uniqid()}><Link smooth to={id}>{item}</Link></li>
                     })}
             </ul>
         </nav>
